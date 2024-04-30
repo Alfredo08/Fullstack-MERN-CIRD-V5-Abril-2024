3da860d81017972853a404e433cdcc36b793898c
Alfredo08 <alfredosv08@gmail.com>
2024-04-30 08:54:54 -0600

    ECMAScript 15

---
+++ Semana 5/Día 1/funcionesFlecha.js
@@ -0,0 +1,10 @@
+console.log(sumaDosNumeros(10, 20));
+
+function sumaDosNumeros(num1, num2){
+    return num1 + num2;
+}
+
+console.log(sumaDosNumerosFlecha(10, 20));
+let sumaDosNumerosFlecha = (num1, num2) => num1 + num2;
+
+
