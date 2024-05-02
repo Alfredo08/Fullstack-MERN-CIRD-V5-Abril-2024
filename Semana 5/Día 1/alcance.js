3bff3a088d4d451863122e64a1a2fa4b788925c5
Alfredo08 <alfredosv08@gmail.com>
2024-05-02 08:33:45 -0600

    Promesas, clases, objetos y herencia

--- Semana 5/Día 1/alcance.js
+++ Semana 5/Día 1/alcance.js
@@ -6,7 +6,7 @@ for(var i = 0; i < 5; i ++){
 console.log(i);
 
 const sumaDosNumeros = (num1, num2) => {
-    var resultado = num1 + num2;
+    let resultado = num1 + num2;
     return resultado;
 }
 
